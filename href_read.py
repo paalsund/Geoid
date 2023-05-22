604f51278aa12d4741fb7b74f78da5fc10f70eaf
Pål Herman Sund <paalsund@gmail.com>
2023-05-22 11:13:53 +0200

    Update href_read.py

--- href_read.py
+++ href_read.py
@@ -1,17 +1,18 @@
 # -*- coding: cp1252 -*-
-# Lese GEOIDE ascii filen fra Kartverket (finnes her, ftp://ftp.geodesi.no/HREF2018A_NN2000_EUREF89.gri)
-# Skriver til en CSV fil som kan prosesseres videre i ArcGIS til en modell/grid
+# Read GEOID ascii file from Kartverket (ftp://ftp.geodesi.no/HREF2018A_NN2000_EUREF89.gri)
+# just writing here to a CSV but may be further processed to a model/grid
 # 
 
-# Filen er organisert i "blokker" med data
-# Blokkene er organisert fra N til S
-# Hver blokk er organisert fra Vest til �st
-# dE som forskjell i �st-verdi mellom hvert datapunkt
-# dN som forskjell i nord-verdi mellom hver "blokk"
+# File organized in "data blocks"
+# Blocks organized from North to South
+# Each block organized West to East
+# dE is delta East between each datapoint
+# dN is delta North between each block
 
 href = open('C:/temp/HREF/HREF2018A_NN2000_EUREF89.gri','r')
 linjer = href.readlines()
 
+# example
 # linjer[0] = '   57.800000   72.000000    4.000000   32.000000   0.0200000   0.0400000'
 # linjer[1] = '\n'
 # linjer[2] = '   9999.000 9999.000 9999.000 9999.000 9999.000 9999.000 9999.000 9999.000\n'
@@ -20,9 +21,9 @@ linjer = href.readlines()
 # linjer[90] = '\n'
 # linjer[91] = '  9999.000 9999.000 9999.000 9999.000 9999.000\n'
 #
-# 9999.000 er NODATA-verdien
+# 9999.000 is the NODATA-value
 
-# Splitter f�rste linje, dvs linjer[0] for � ta ut parametre
+# Split first, i.e linjer[0] for grabbing the file parameters
 parametre = linjer[0].split()
 S =  float(parametre[0])
 N =  float(parametre[1])
@@ -31,36 +32,35 @@ E =  float(parametre[3])
 dN = float(parametre[4])
 dE = float(parametre[5])
 
-# Subliste kun for dataverdiene
+# Sublist for the actual data
 datalinjer = linjer[2:]
-# datalinjer er en liste, der hver rad i lista er en streng
-# blokkene har strenger med lengde 46 eller 73
-# blokkene skilles av streng '\n' med lengde 1 (en)
+# datalinjer is a list, each line is a string
+# blocks are stringths with length 46 or 73
+# blocks are separated by '\n' (length 1)
 
 
-# Lager en fil for � skrive resultatene til
+# File for writing
 resultatfil = open('C:/temp/HREF/test2.csv','w')
 resultatfil.write('Lat Long GeoideH\n')
 
 
-# Loop over blokkene
+# Loop over blocks
 Nord = N
 Ost = W
 for linje in datalinjer:
-    # len(linje) > 1 for alle linjene i en blokk
-    # I hver blokk er Nord-verdien konstant
-    # men hvert datapunkt skal inkrementeres fra Vest
-    # med verdien dE
+    # len(linje) > 1 for all lines in a block
+    # For each bloch the North/N is constant
+    # for each datapoint East is incremented by dE
     if len(linje) > 1:
         words = linje.split()
         for word in words:
             GeoideH = float(word)
             resultatfil.write('%5.3f %5.3f %5.3f\n' % (Nord, Ost, GeoideH))
-            ## print Nord, Ost, GeoideH
+            ## print North, East, GeoideH
             Ost = Ost + dE
-    # len(linje) = 1 for linjen som skiller hver blokk
-    # Resetter da �stverdien til utgangspunktet W
-    # Nordverdien dekrementeres med -dN 
+    # len(linje) = 1 for the line separating each block
+    # Resetting the Eastvalue to W
+    # North value decremented by -dN 
     if len(linje) == 1:
         Nord = Nord - dN
         Ost = W
